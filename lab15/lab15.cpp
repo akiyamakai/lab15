d0048ea0c1c9ac23a7b0eb71debcdef3c222451d
grush <grush@DESKTOP-P5A2MM5>
2024-05-06 14:47:57 +0300

    Добавьте файлы проекта.

---
+++ lab15/lab15.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    const int size = 20;
+    int a[size], b[size];
+
+
+    cout << "Enter " << size << " integer numbers:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cin >> a[i];
+    }
+
+
+    int b_size = 0;
+    for (int i = 0; i < size; ++i) {
+        if (a[i] < 0) {
+            b[b_size] = a[i];
+            b_size++;
+        }
+    }
+
+
+    sort(b, b + b_size);
+
+
+    cout << "Sorted array of negative elements from array a:" << endl;
+    for (int i = 0; i < b_size; ++i) {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
