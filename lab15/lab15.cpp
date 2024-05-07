c83fec5ec908764e87b24f818e08bd468a0db6d3
Vitaliy Pismak 723A <97937864+akiyamakai@users.noreply.github.com>
2024-05-07 12:11:40 +0300

    Update lab15.cpp

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -1,18 +1,27 @@
-﻿#include <iostream>
-#include <algorithm>
+#include <iostream>
 
 using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int main() {
     const int size = 20;
     int a[size], b[size];
 
-
     cout << "Enter " << size << " integer numbers:" << endl;
     for (int i = 0; i < size; ++i) {
         cin >> a[i];
     }
-
 
     int b_size = 0;
     for (int i = 0; i < size; ++i) {
@@ -22,9 +31,7 @@ int main() {
         }
     }
 
-
-    sort(b, b + b_size);
-
+    bubbleSort(b, b_size);
 
     cout << "Sorted array of negative elements from array a:" << endl;
     for (int i = 0; i < b_size; ++i) {
